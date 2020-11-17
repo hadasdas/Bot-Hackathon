db9fbdde5490b4030bb07ff0581685d8678f76ec
Hadas Sonnenfeld <hadas.schacham@mail.huji.ac.il>
2020-11-17 09:58:56 +0200

    adding initial bot class so it can run

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response, request
 import requests
-from tele_bot import bot_flow
+from tele_bot import Bot
 from config import TOKEN, TELEGRAM_INIT_WEBHOOK_URL
 
 
@@ -14,7 +14,12 @@ def handle_message():
     req = request.get_json()
     chat_id = req['message']['chat']['id']
     text = str(req['message']['text'])
-    bot_answer = bot_flow(text)
+    my_bot = Bot()
+    bot_answer = my_bot.bot_flow(text)
     requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}"
                  .format(TOKEN, chat_id, bot_answer))
     return Response("success")
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
---
+++ tele_bot.py
@@ -0,0 +1,6 @@
+class Bot:
+    def __init__(self):
+        pass
+
+    def bot_flow(self, text):
+        return "Hello World"
