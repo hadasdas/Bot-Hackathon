933a89bd17b9881f6f4d98adaad29a1c6fbd17bc
Hadas Sonnenfeld <hadas.schacham@mail.huji.ac.il>
2020-11-19 12:25:07 +0200

    playing with graphics of bot's reactions

--- bot_speech.py
+++ bot_speech.py
@@ -1,28 +1,31 @@
 speech_dict = {"welcome": f"{chr(0x1F984)} hi there! {chr(0x1F338)}\n",
                "see again": f"{chr(0x1F984)} good to see you again! {chr(0x1F917)}{chr(0x1F917)}\n",
-               "mood": f"{chr(0x1F984)} How are you feeling today? Press 1 if you're in a good mood {chr(0x1F31E)}, "
-                       f"\nor 2 if you're in a bad mood today {chr(0x1F328)}",
-               "reaction to happy": f"{chr(0x1F984)} Glad to see all well with you! \nLet's see if we can keep the spirits high...",
+               "mood": f"{chr(0x1F984)} How are you feeling today? "
+                       f"\nPress {chr(0x1F449)} 1 if you're in a good mood {chr(0x1F31E)}, "
+                       f"\nor {chr(0x1F449)} 2 if you're in a bad mood today {chr(0x1F328)}",
+               "reaction to happy": f"{chr(0x1F984)} Glad to see all well with you! \nLet's see if we can keep the spirits high...\n",
 
-               "reaction to sad": f"{chr(0x1F984)} How can I cheer you up? \nI think I might have something up my sleeve...",
-               "menu": f"\nIf you'd like to hear a hilarious joke {chr(0x1F923)} - enter 1,"
-                       f"\nif you want to listen to music {chr(0x1F3B5)} - enter 2,"
-                       f"\nif you'd like to see a cute picture of a kitten {chr(0x1F970)} - enter 3,"
-                       f"\nif you'd like to join a hearty laughter {chr(0x1F602)} - enter 4,"
-                       f"\nif you'd like to shuffle randomly between all the fun things I can do for you - enter 5"
-                       f"\nor enter 6 if you'd like to say goodbye {chr(0x1F614)}",
-               "bot is waiting": f"{chr(0x1F984)} I am a patient bot, and I'm already waiting for your next request "
+               "reaction to sad": f"{chr(0x1F984)} How can I cheer you up? \nI think I might have something up my sleeve...\n",
+               "menu": f"\n{chr(0x1F449)}If you'd like to hear a hilarious joke {chr(0x1F923)} - enter 1"
+                       f"\n{chr(0x1F449)}want to listen to music {chr(0x1F3B5)} - enter 2"
+                       f"\n{chr(0x1F449)}to see a cute picture of a kitten {chr(0x1F63B)} - enter 3 "
+                       f"\n{chr(0x1F449)}to join a hearty laughter {chr(0x1F602)} - enter 4 "
+                       f"\n{chr(0x1F449)}to shuffle randomly between all the fun things I can do for you - enter 5"
+                       f"\n{chr(0x1F449)}or enter 6  if you'd like to say goodbye {chr(0x1F614)}",
+               "bot is waiting": f"{chr(0x1F984)} I am a patient bot, and I'm already waiting for your next request \n"
                                  "\n(1 for joke, 2 for song, 3 for cat pic, 4 for laughter, 5 for a random shuffle, "
                                  "6 - see ya 'round)\n",
                "choose music": f"{chr(0x1F984)} I think you might want some upbeat cheerful music, {chr(0x1F4AB)}"
                                f"\nbut you might want just relaxing, mellow music to match your mood {chr(0x1F342)}."
                                f" So I'm letting you decide:"
-                               "\nPress 7 if you want upbeat music from your cheerApp bot"
-                               "\nor 8 if you'd like it on the quiet side",
+                               f"\nPress 7 {chr(0x1F448)} if you want upbeat music from your cheerApp bot"
+                               f"\nor 8 {chr(0x1F448)} if you'd like it on the quiet side",
                "cat awh": f"\n{chr(0x1F984)} AWHHH! So cute!! {chr(0x1F63B)}",
-               "get feedback": f"{chr(0x1F984)} I'd like to get your feedback on the feature I just picked out for you "
-                               f"some thought about your preferences - {chr(0x1F914)}"
-                               "\nIf you enjoyed it press 1, if you would have preferred something else - press 2",
+               "get feedback": f"{chr(0x1F984)} I was thinking about you and the features you prefer when I chose "
+                               f"this last surprise for you {chr(0x1F913)}{chr(0x1F60D)}. "
+                               f"\nI'd like to get your feedback on that feature."
+                               f"\nIf you enjoyed it press 1 {chr(0x1F448)}, "
+                               f"\nif you would have preferred something else - press 2 {chr(0x1F448)}",
                "good bye": f"{chr(0x1F984)} It was great hearing from you, I'll be here next time you drop by \nGood bye!"}
 
 
